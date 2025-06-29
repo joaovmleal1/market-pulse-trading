2e56691768ae3d907be3c21a5b9648b2d44ab9b9
Pedro Medeiros <pedro.contato166@hotmail.com>
2025-06-29 02:06:50 -0300

    Adjusting DashHistory

--- src/pages/DashHistory.tsx
+++ src/pages/DashHistory.tsx
@@ -18,7 +18,7 @@ const DashHistory = () => {
 
   const fetchTrades = async () => {
     try {
-      const res = await fetch(`https://api.multitradingob.com/trade-order-info/trade_order_info/today/1`, {
+      const res = await fetch(`https://api.multitradingob.com/trade-order-info/trade_order_info/all}`, {
         headers: { Authorization: `Bearer ${accessToken}`, 'Content-Type': 'application/json' },
       });
       if (!res.ok) throw new Error('Erro ao buscar operações');
